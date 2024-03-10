16d8ce067ecc5c4b38e018d38beb3362caccce4b
izgin <izginsaglam@hotmail.com>
2024-03-10 22:27:10 +0300

    Partail AdminLayout

---
+++ izginn/Controllers/SkillController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace izginn.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager skillManager = new SkillManager(new EfSkill());
+        public IActionResult Index()
+        {
+            var value = skillManager.TGetList();
+            return View(value);
+        }
+
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+    }
+}
